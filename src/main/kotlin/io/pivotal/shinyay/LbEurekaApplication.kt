24caa277aac9f765e47f7786468bffec3a853be1
shinyay <shinya.com@gmail.com>
2019-08-31 13:07:05 +0900

    Modify main application class to execute each component

--- src/main/kotlin/io/pivotal/shinyay/LbEurekaApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/LbEurekaApplication.kt
@@ -1,11 +1,25 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.registration.EurekaServer
+import io.pivotal.shinyay.registration.runEurekaServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+//@SpringBootApplication
 class LbEurekaApplication
 
 fun main(args: Array<String>) {
-	runApplication<LbEurekaApplication>(*args)
+	var serverName = ""
+
+	when(args.size) {
+		2 -> {
+			System.setProperty("server.port", args[1])
+			serverName = args[0].toLowerCase()
+		}
+		1 -> serverName = args[0].toLowerCase()
+	}
+
+	when(serverName) {
+		"eureka" -> runEurekaServer(args)
+	}
 }
