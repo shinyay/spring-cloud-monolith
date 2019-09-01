ed4b175384545d71c4a30cae26a722a49661a3b0
shinyay <shinya.com@gmail.com>
2019-09-01 12:56:14 +0900

    Add subtraction service call to controller

--- src/main/kotlin/io/pivotal/shinyay/loadbalancer/controller/WebCountController.kt
+++ src/main/kotlin/io/pivotal/shinyay/loadbalancer/controller/WebCountController.kt
@@ -2,6 +2,7 @@ package io.pivotal.shinyay.loadbalancer.controller
 
 import io.pivotal.shinyay.domain.subtraction.SubtractionServer
 import io.pivotal.shinyay.loadbalancer.service.WebAdditionService
+import io.pivotal.shinyay.loadbalancer.service.WebSubtractionService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class WebCountController(val webAdditionService: WebAdditionService) {
+class WebCountController(val webAdditionService: WebAdditionService,
+                         val webSubtractionService: WebSubtractionService) {
 
     val logger: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
@@ -23,5 +25,16 @@ class WebCountController(val webAdditionService: WebAdditionService) {
         logger.info("SUM: $sum")
         model.addAttribute("json", sum)
         return "sum"
+    }
+
+    @RequestMapping("/sub")
+    fun sub(@RequestParam(defaultValue = "0") param1: String,
+            @RequestParam(defaultValue = "0") param2: String,
+            model: Model): String {
+
+        val diff = webSubtractionService.sub(param1, param2)
+        logger.info("DIFF: $diff")
+        model.addAttribute("json", diff)
+        return "diff"
     }
 }
